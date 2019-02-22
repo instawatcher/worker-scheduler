f9cccc29a4c3002ba711bfaf1895c47d22221dc9
Matthew Coal <matemate001@gmail.com>
2019-02-22 17:56:30 +0100

    chore: Add todo for test

--- test/main.js
+++ test/main.js
@@ -5,4 +5,5 @@ const Scheduler = require('../lib/index');
 it('should resolve shit', (done) => {
   const sched = new Scheduler();
   sched.addWorker(new Scheduler.Worker('woot', require.resolve('./assets/worker1'), 1000));
+  // TODO: Finish test
 });
